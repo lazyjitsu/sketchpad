10795cf2da6d7d06962938801fd6ce28c8ffc6de
Marko Cienega <graphneenja@gmail.com>
2024-07-30 15:54:46 -0700

    second commit

---
+++ js/math.js
@@ -0,0 +1,5 @@
+const math = {};
+
+math.subtract = (p1, p2) => {
+  return [p1[0] - p2[0], p1[1] - p2[1]];
+};
--- js/sketchPad.js
+++ js/sketchPad.js
@@ -4,7 +4,45 @@ class SketchPad {
     this.spc = document.createElement("canvas");
     this.spc.width = size;
     this.spc.height = size;
-    this.spc.style = `background-color:white`;
+    this.spc.style = `
+      background-color:white;
+      box-shadow: 0px 0px 10px 2px black;
+    `;
     container.appendChild(this.spc);
+
+    this.ctx = this.spc.getContext("2d");
+
+    this.isDrawing = false;
+    this.paths = [];
+
+    this.#startListeners();
   }
+  #startListeners() {
+    this.spc.onmousedown = (evt) => {
+      const mouse = this.#getMouse(evt);
+      this.paths = [mouse];
+      this.isDrawing = true;
+    };
+    this.spc.onmousemove = (evt) => {
+      if (this.isDrawing) {
+        const mouse = this.#getMouse(evt);
+        this.paths.push(mouse);
+        this.#redraw();
+      }
+    };
+    this.spc.onmouseup = () => {
+      this.isDrawing = false;
+    };
+  }
+  #getMouse = (e) => {
+    const rect = this.spc.getBoundingClientRect();
+    return [
+      Math.round(e.clientX - rect.left),
+      Math.round(e.clientY - rect.top),
+    ];
+  };
+  #redraw = () => {
+    this.ctx.clearRect(0, 0, this.spc.width, this.spc.height);
+    draw.path(this.ctx, this.paths);
+  };
 }
